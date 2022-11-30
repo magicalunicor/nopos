6123b692feb569c5bbd80138e5118af90c4ef5ab
magicalunicor <101104515+magicalunicor@users.noreply.github.com>
2022-11-30 11:52:57 +0100

    Added "working" markers

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-import { React, useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import { React, useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as BarData from "../data/features.json";
+
 export default function Home() {
+  const [selectedBar, setSelectedBar] = useState();
+
   const [viewport, setViewport] = useState({
     latitude: 45.464664,
     longitude: 9.18854,
@@ -9,10 +13,17 @@ export default function Home() {
     zoom: 12,
   });
 
+  useEffect(() => {
+    if (selectedBar) {
+      console.log("NAME: " + selectedBar.properties.name);
+      console.log("ADDRESS: " + selectedBar.properties.full_address);
+      console.log("ID: " + selectedBar.id);
+    }
+  }, [selectedBar]);
+
   return (
     <div className="flex h-screen w-screen">
       <ReactMapGL
-      onClick={(e)=>(console.log(e))}
         {...viewport}
         onMove={(viewport) => {
           setViewport(viewport);
@@ -20,6 +31,23 @@ export default function Home() {
         mapStyle={"mapbox://styles/caporetto/clb3eipel000215qvgt65l533"}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_ACCESS_TOKEN}
       >
+        {BarData.features.map((bar) => (
+          <Marker
+            pitchAlignment="viewport"
+            key={bar.id}
+            latitude={bar.geometry.coordinates[1]}
+            longitude={bar.geometry.coordinates[0]}
+          >
+            <div
+              onClick={() => {
+                setSelectedBar(bar);
+              }}
+              className="flex w-fit h-fit p-1 rounded-md bg-white border border-black cursor-pointer"
+            >
+              <h2 className="font-extrabold">{bar.properties.name}</h2>
+            </div>
+          </Marker>
+        ))}
       </ReactMapGL>
     </div>
   );
