750947ef8542b0bd1e7c57fb999ea83048a35f26
magicalunicor <101104515+magicalunicor@users.noreply.github.com>
2022-11-30 10:10:40 +0100

    Added Mapbox

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-
-
+import { React, useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 export default function Home() {
+  const [viewport, setViewport] = useState({
+    latitude: 45.464664,
+    longitude: 9.188540,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
+
   return (
-    <div>
-      <h2>hello world</h2>
+    <div className="flex h-screen w-screen">
+      <ReactMapGL
+        {...viewport}
+        onMove={(viewport) => {
+          setViewport(viewport);
+        }}
+        mapStyle={"mapbox://styles/caporetto/clb3eipel000215qvgt65l533"}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_ACCESS_TOKEN}
+      />
     </div>
-  )
+  );
 }
