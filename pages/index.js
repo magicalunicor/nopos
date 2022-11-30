d882affe595a9f92da44fccb629214b33db4e98e
magicalunicor <101104515+magicalunicor@users.noreply.github.com>
2022-11-30 10:48:08 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import ReactMapGL, { Marker } from "react-map-gl";
 export default function Home() {
   const [viewport, setViewport] = useState({
     latitude: 45.464664,
-    longitude: 9.188540,
+    longitude: 9.18854,
     width: "100vw",
     height: "100vh",
-    zoom: 10,
+    zoom: 12,
   });
 
   return (
     <div className="flex h-screen w-screen">
       <ReactMapGL
+      onClick={(e)=>(console.log(e))}
         {...viewport}
         onMove={(viewport) => {
           setViewport(viewport);
         }}
         mapStyle={"mapbox://styles/caporetto/clb3eipel000215qvgt65l533"}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_ACCESS_TOKEN}
-      />
+      >
+      </ReactMapGL>
     </div>
   );
 }
